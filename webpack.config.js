b07dc14ec7eee9c824e02ea564b189929e4b521a
thinceller <kohei.ct@icloud.com>
2018-07-11 12:23:59 +0900

    main processのwebpack.configを分割

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const distDir = path.join(__dirname, 'dist');
 
-module.exports = [
-  // main process bundle
-  {
-    entry: './src/main.js',
-    target: 'electron-main',
-    node: {
-      __dirname: false,
-    },
-    output: {
-      path: distDir,
-      filename: 'main.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['env', {'modules': false}],
-                ],
-              },
-            }
-          ]
-        }
-      ],
-    },
-  },
   // renderer process bundle
   {
     entry: './src/renderer/index.jsx',
